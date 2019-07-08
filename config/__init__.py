b85d2803dbbe8dd8516e03afbcd40fed2bf7bbb0
Djordje <djordjeponjavic@gmail.com>
2019-07-08 16:25:05 +0200

    zavrsen day 1

---
+++ config/__init__.py
@@ -0,0 +1,12 @@
+class Config:
+    ENV_TYPE = None
+    
+    DEBUG = False
+    FLASK_HOST = '127.0.0.1'
+    FLASK_PORT = 8000
+
+    DB_NAME = None
+    DB_USER = None
+    DB_PASSWD = None
+    DB_HOST = None
+    DB_PORT = None
--- config/development.py
+++ config/development.py
@@ -1,2 +1,12 @@
-class Development:
+from . import Config as Config
+
+class Development(Config):
+    EVN_TYPE = "Dev"
+
+    DB_NAME = "temperature"
+    DB_USER =  "ognjen"
+    DB_PASSWD = "praksa"
+    DB_HOST = "127.0.0.1"
+    DB_PORT = 5432
+
     pass
